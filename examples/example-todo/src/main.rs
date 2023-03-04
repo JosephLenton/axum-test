4b5dda32937eb0fb59e5e57b4034597c73903070
josephlenton@gmail.com <josephlenton@gmail.com>
2023-03-04 20:12:02 +0000

    docs: add sample todo

--- examples/example-todo/src/main.rs
+++ examples/example-todo/src/main.rs
@@ -1,9 +1,13 @@
 //!
-//! This is an example Todo Application.
-//! To show how one can write some simple tests.
+//! This is an example Todo Application to show some simple tests.
 //!
-//! It's all one file, and kept to the absolutely bare minimum.
-//! Scroll to the bottom to see the tests.
+//! The app includes the end points for ...
+//!
+//!  - POST /login ... this takes an email, and returns a session cookie.
+//!  - PUT /todo ... once logged in, one can store todos.
+//!  - GET /todo ... once logged in, you can retrieve all todos you have stored.
+//!
+//! At the bottom of this file are a series of tests for these endpoints.
 //!
 
 use ::anyhow::anyhow;
@@ -27,13 +31,20 @@ use ::std::net::IpAddr;
 use ::std::net::Ipv4Addr;
 use ::std::net::SocketAddr;
 use ::std::result::Result as StdResult;
+use ::std::sync::Arc;
+use ::std::sync::RwLock;
+
+#[cfg(test)]
+use ::axum_test::TestServer;
+#[cfg(test)]
+use ::axum_test::TestServerConfig;
 
 const PORT: u16 = 8080;
 const USER_ID_COOKIE_NAME: &'static str = &"example-todo-user-id";
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let result = {
+async fn main() {
+    let result: Result<()> = {
         let app = new_app();
 
         // Start!
@@ -48,35 +59,41 @@ async fn main() -> Result<()> {
         Err(err) => eprintln!("{}", err),
         _ => {}
     };
-
-    result
 }
+
+type SharedAppState = Arc<RwLock<AppState>>;
 
 // This my poor mans in memory DB.
-#[derive(Clone, Debug)]
+#[derive(Debug)]
 pub struct AppState {
-    user_todos: HashMap<u32, Vec<String>>,
+    user_todos: HashMap<u32, Vec<Todo>>,
 }
 
-pub(crate) fn new_app() -> IntoMakeService<Router> {
-    let state = AppState {
-        user_todos: HashMap::new(),
-    };
-
-    let router: Router = Router::new()
-        .route(&"/login", post(route_post_user_login))
-        .route(&"/todo", get(route_get_user_todos))
-        .route(&"/todo", put(route_put_user_todos))
-        .with_state(state);
-
-    router.into_make_service()
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq)]
+pub struct Todo {
+    name: String,
+    content: String,
 }
 
-// Never do something like this in a real application.
-// It's really bad. Like _seriously_ bad.
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct LoginRequest {
+    user: Email,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct AllTodos {
+    todos: Vec<Todo>,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct NumTodos {
+    num: u32,
+}
+
+// Note you should never do something like this in a real application
+// for session cookies. It's really bad. Like _seriously_ bad.
 //
-// Do stuff like this and users can login as each other.
-// You are asking for trouble.
+// This is done like this here to keep the code shorter. That's all.
 fn get_user_id_from_cookie(cookies: &CookieJar) -> Result<u32> {
     cookies
         .get(&USER_ID_COOKIE_NAME)
@@ -85,49 +102,242 @@ fn get_user_id_from_cookie(cookies: &CookieJar) -> Result<u32> {
         .ok_or_else(|| anyhow!("id not found"))
 }
 
-#[derive(Debug, Clone, Deserialize, Serialize)]
-pub struct LoginRequest {
-    user: Email,
+pub async fn route_post_user_login(
+    State(ref mut state): State<SharedAppState>,
+    mut cookies: CookieJar,
+    Json(_body): Json<LoginRequest>,
+) -> CookieJar {
+    let mut lock = state.write().unwrap();
+    let user_todos = &mut lock.user_todos;
+    let user_id = user_todos.len() as u32;
+    user_todos.insert(user_id, vec![]);
+
+    let really_insecure_login_cookie = Cookie::new(USER_ID_COOKIE_NAME, user_id.to_string());
+    cookies = cookies.add(really_insecure_login_cookie);
+
+    cookies
 }
 
-pub async fn route_post_user_login(
-    State(ref mut state): State<AppState>,
+pub async fn route_put_user_todos(
+    State(ref mut state): State<SharedAppState>,
     mut cookies: CookieJar,
-    Json(body): Json<LoginRequest>,
-) -> StdResult<CookieJar, StatusCode> {
+    Json(todo): Json<Todo>,
+) -> StdResult<Json<u32>, StatusCode> {
     get_user_id_from_cookie(&mut cookies)
         .map(|user_id| {
-            let user_id = state.user_todos.len() as u32;
-            state.user_todos.insert(user_id, vec![]);
+            let mut lock = state.write().unwrap();
+            let maybe_todos = lock.user_todos.get_mut(&user_id);
 
-            let really_insecure_login_cookie =
-                Cookie::new(USER_ID_COOKIE_NAME, user_id.to_string());
-            cookies = cookies.add(really_insecure_login_cookie);
+            let todos = maybe_todos.unwrap();
+            todos.push(todo);
+            let num_todos = todos.len() as u32;
 
-            cookies
+            Json(num_todos)
         })
         .map_err(|_| StatusCode::UNAUTHORIZED)
 }
 
 pub async fn route_get_user_todos(
-    State(ref state): State<AppState>,
-    cookies: CookieJar,
-) -> StdResult<(), StatusCode> {
-    let result = ();
+    State(ref state): State<SharedAppState>,
+    mut cookies: CookieJar,
+) -> StdResult<Json<Vec<Todo>>, StatusCode> {
+    get_user_id_from_cookie(&mut cookies)
+        .map(|user_id| {
+            let lock = state.read().unwrap();
+            let todos = lock.user_todos[&user_id].clone();
 
-    Ok(result)
+            Json(todos)
+        })
+        .map_err(|_| StatusCode::UNAUTHORIZED)
 }
 
-pub async fn route_put_user_todos(
-    State(ref state): State<AppState>,
-    cookies: CookieJar,
-) -> StdResult<(), StatusCode> {
-    let result = ();
+pub(crate) fn new_app() -> IntoMakeService<Router> {
+    let state = AppState {
+        user_todos: HashMap::new(),
+    };
+    let shared_state = Arc::new(RwLock::new(state));
 
-    Ok(result)
+    let router: Router = Router::new()
+        .route(&"/login", post(route_post_user_login))
+        .route(&"/todo", get(route_get_user_todos))
+        .route(&"/todo", put(route_put_user_todos))
+        .with_state(shared_state);
+
+    router.into_make_service()
+}
+
+#[cfg(test)]
+fn new_test_app() -> TestServer {
+    let app = new_app();
+
+    TestServer::new_with_config(
+        app,
+        TestServerConfig {
+            // Preserve cookies across requests
+            // for the session cookie to work.
+            save_cookies: true,
+            ..TestServerConfig::default()
+        },
+    )
+    .unwrap()
 }
 
 #[cfg(test)]
 mod test_post_login {
-    // todo
+    use super::*;
+
+    use ::serde_json::json;
+
+    #[tokio::test]
+    async fn it_should_create_session_on_login() {
+        let server = new_test_app();
+
+        let response = server
+            .post(&"/login")
+            .json(&json!({
+                "user": "my-login@example.com",
+            }))
+            .await;
+
+        let session_cookie = response.cookie(&USER_ID_COOKIE_NAME);
+        assert_ne!(session_cookie.value(), "");
+    }
+
+    #[tokio::test]
+    async fn it_should_not_login_using_non_email() {
+        let server = new_test_app();
+
+        let response = server
+            .post(&"/login")
+            .json(&json!({
+                "user": "blah blah blah",
+            }))
+            .expect_failure()
+            .await;
+
+        // There should not be a session created.
+        let cookie = response.maybe_cookie(&USER_ID_COOKIE_NAME);
+        assert!(cookie.is_none());
+    }
+}
+
+#[cfg(test)]
+mod test_route_put_user_todos {
+    use super::*;
+
+    use ::serde_json::json;
+
+    #[tokio::test]
+    async fn it_should_not_store_todos_without_login() {
+        let server = new_test_app();
+
+        let response = server
+            .put(&"/todo")
+            .json(&json!({
+                "name": "shopping",
+                "content": "buy eggs",
+            }))
+            .expect_failure()
+            .await;
+
+        assert_eq!(response.status_code(), StatusCode::UNAUTHORIZED);
+    }
+
+    #[tokio::test]
+    async fn it_should_return_number_of_todos_as_more_are_pushed() {
+        let server = new_test_app();
+
+        server
+            .post(&"/login")
+            .json(&json!({
+                "user": "my-login@example.com",
+            }))
+            .await;
+
+        let num_todos = server
+            .put(&"/todo")
+            .json(&json!({
+                "name": "shopping",
+                "content": "buy eggs",
+            }))
+            .await
+            .json::<u32>();
+        assert_eq!(num_todos, 1);
+
+        let num_todos = server
+            .put(&"/todo")
+            .json(&json!({
+                "name": "afternoon",
+                "content": "buy shoes",
+            }))
+            .await
+            .json::<u32>();
+        assert_eq!(num_todos, 2);
+    }
+}
+
+#[cfg(test)]
+mod test_route_get_user_todos {
+    use super::*;
+
+    use ::serde_json::json;
+
+    #[tokio::test]
+    async fn it_should_not_return_todos_if_logged_out() {
+        let server = new_test_app();
+
+        let response = server
+            .put(&"/todo")
+            .json(&json!({
+                "name": "shopping",
+                "content": "buy eggs",
+            }))
+            .expect_failure()
+            .await;
+
+        assert_eq!(response.status_code(), StatusCode::UNAUTHORIZED);
+    }
+
+    #[tokio::test]
+    async fn it_should_return_all_todos_when_logged_in() {
+        let server = new_test_app();
+
+        server
+            .post(&"/login")
+            .json(&json!({
+                "user": "my-login@example.com",
+            }))
+            .await;
+
+        // Push two todos.
+        server
+            .put(&"/todo")
+            .json(&json!({
+                "name": "shopping",
+                "content": "buy eggs",
+            }))
+            .await;
+        server
+            .put(&"/todo")
+            .json(&json!({
+                "name": "afternoon",
+                "content": "buy shoes",
+            }))
+            .await;
+
+        // Get all todos out from the server.
+        let todos = server.get(&"/todo").await.json::<Vec<Todo>>();
+
+        let expected_todos: Vec<Todo> = vec![
+            Todo {
+                name: "shopping".to_string(),
+                content: "buy eggs".to_string(),
+            },
+            Todo {
+                name: "afternoon".to_string(),
+                content: "buy shoes".to_string(),
+            },
+        ];
+        assert_eq!(todos, expected_todos)
+    }
 }
