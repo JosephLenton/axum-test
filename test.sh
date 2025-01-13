c6d8b39ab6f29b306d2d4dabc55be64df5f64326
Joseph Lenton <JosephLenton@users.noreply.github.com>
2025-01-13 18:21:46 +0000

    feat: add nightly test (#135)

--- test.sh
+++ test.sh
@@ -10,6 +10,9 @@ cargo +stable test --example=example-websocket-chat --features ws
 cargo +stable test  --features all "$@"
 cargo +stable test "$@"
 
+# Check nightly also works, see https://github.com/JosephLenton/axum-test/issues/133
+cargo +nightly test  --features all "$@"
+
 # Check the various build variations work
 cargo +stable check --no-default-features
 cargo +stable check --features all
