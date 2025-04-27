9b90aa5fbd590fb0c43a4211942536c444d657b8
Joseph Lenton <JosephLenton@users.noreply.github.com>
2025-04-27 20:33:00 +0100

    feat: add private expect json support (#145)
    * feat: add expect_json support for initial release
    
    * chore: update to expect json rc1
    
    * fix: use old diff when enabled

---
+++ src/expect_json.rs
@@ -0,0 +1,18 @@
+pub use ::expect_json::expect;
+pub use ::expect_json::expect_op_for_axum_test as expect_op;
+pub use ::expect_json::ops;
+pub use ::expect_json::Context;
+pub use ::expect_json::ExpectJsonError;
+pub use ::expect_json::ExpectJsonResult;
+pub use ::expect_json::ExpectOp;
+pub use ::expect_json::ExpectOpError;
+pub use ::expect_json::ExpectOpExt;
+pub use ::expect_json::ExpectOpResult;
+pub use ::expect_json::JsonType;
+
+#[doc(hidden)]
+pub use ::expect_json::ExpectOpSerialize;
+#[doc(hidden)]
+pub use ::expect_json::SerializeExpectOp;
+#[doc(hidden)]
+pub use ::expect_json::__private;
---
+++ tests/test-expect-json-integration.rs
@@ -0,0 +1,14 @@
+use axum_test::expect_json::expect_op;
+use axum_test::expect_json::ExpectOp;
+
+// If it compiles, it works!
+#[test]
+fn test_expect_op_for_axum_test_integration_compiles() {
+    #[expect_op]
+    #[derive(Debug, Clone)]
+    pub struct Testing;
+
+    impl ExpectOp for Testing {}
+
+    assert!(true);
+}
